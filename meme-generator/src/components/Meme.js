6161b934aced2d1f681ebea9b4079dcf28a91819
Henry Derbyshire <henry@htderbyshire.com>
2022-02-09 13:40:55 +0000

    converted to async function

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -10,10 +10,13 @@ function Meme() {
   const [allMemes, setAllMemes] = React.useState();
 
   React.useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then(res => res.json())
-      .then(data => setAllMemes(data.data.memes))
-    console.log("memes")
+    async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setAllMemes(data.data.memes);
+    }
+
+    getMemes()
   }, [])
 
   function getMemeImage() {
