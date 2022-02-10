21d4658757afd5a7eb7d0d2ef13e35360cb40644
Henry Derbyshire <henry@htderbyshire.com>
2022-02-10 08:36:58 +0000

    created all new dice function

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './App.css';
 import Die from './Die';
 
+
 function App() {
+
+  function allNewDice() {
+    const numberArray = [];
+    for (let i = 0; i < 10; i++) {
+      numberArray.push(Math.floor((Math.random()) * 6) + 1);
+    }
+    return numberArray;
+  }
+
   return (
     <main>
       <div className="dice-container">
