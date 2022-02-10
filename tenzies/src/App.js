e13a3492964483b4601b9e55a59f8f028a3d4651
Henry Derbyshire <henry@htderbyshire.com>
2022-02-10 11:33:37 +0000

    wired new game button, finished project

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Die from './Die';
 import {nanoid} from 'nanoid';
-
+import Confetti from 'react-confetti';
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
@@ -50,21 +50,27 @@ function App() {
   })
 
   function rollDice() {
-    setDice(oldDice => oldDice.map(die => {
-      return die.isHeld === false 
-        ? generateNewDie()
-        : die
-    }))
+    if (tenzies) {
+      setDice(allNewDice());
+      setTenzies(false);
+    } else {
+      setDice(oldDice => oldDice.map(die => {
+        return die.isHeld === false 
+          ? generateNewDie()
+          : die
+      }))
+    }
   }
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
-      <button onClick={rollDice} className="roll-dice">Roll!</button>
+      <button onClick={rollDice} className="roll-dice">{tenzies ? "New Game" : "Roll!"}</button>
     </main>
   );
 }
