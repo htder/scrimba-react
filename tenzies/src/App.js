da278fa2b12ba3e41d1dd33d7f039861f1597bbf
Henry Derbyshire <henry@htderbyshire.com>
2022-02-10 11:15:27 +0000

    added fucntion to test if the game is complete

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -6,6 +6,17 @@ import {nanoid} from 'nanoid';
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+
+  React.useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every(die => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      console.log("you won!")
+    }
+  }, [dice])
 
   function allNewDice() {
     const numberArray = [];
