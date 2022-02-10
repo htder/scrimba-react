662ccf3fd27ffe6f17d453b4bfcf5fa6107e6c9a
Henry Derbyshire <henry@htderbyshire.com>
2022-02-10 10:39:37 +0000

    added instructional text to game and ability to hold dice

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,31 +1,55 @@
 import React from 'react';
 import './App.css';
 import Die from './Die';
+import {nanoid} from 'nanoid';
 
 
 function App() {
-  const [dice, setDice] = useState(allNewDice());
+  const [dice, setDice] = React.useState(allNewDice());
 
   function allNewDice() {
     const numberArray = [];
     for (let i = 0; i < 10; i++) {
-      numberArray.push(Math.floor((Math.random()) * 6) + 1);
+      numberArray.push(generateNewDie());
     }
     return numberArray;
   }
 
-  const diceElements = dice.map((item, index) => {
+  function generateNewDie() {
+    return {
+        value: Math.floor((Math.random()) * 6) + 1,
+        isHeld: false,
+        id: nanoid(),
+    }
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) => oldDice.map(die => {
+        return die.id === id 
+          ? {...die, isHeld: !die.isHeld}
+          : die
+      })
+    )
+  }
+
+  const diceElements = dice.map(item => {
     return (
-      <Die value={item} key={index}/>
+      <Die value={item.value} key={item.id} isHeld={item.isHeld} holdDice={() => holdDice(item.id)}/>
     )
   })
 
   function rollDice() {
-    setDice(allNewDice());
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld === false 
+        ? generateNewDie()
+        : die
+    }))
   }
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
--- tenzies/src/Die.js
+++ tenzies/src/Die.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function Die(props) {
+  const styles = {
+    backgroundColor: "#59E391",
+  }
   return (
-    <div className="die-face">
+    <div 
+      className="die-face" 
+      style={props.isHeld ? styles : {}} 
+      onClick={props.holdDice}
+    >
       <h2 className="die-num">{props.value}</h2>
     </div>
   )
